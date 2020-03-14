0a28232a0e9323b0da683d0c76ac562172a355fc
akshay-toshniwal <60028842+akshay-toshniwal@users.noreply.github.com>
2020-03-14 15:56:58 +0530

    second commit

--- navigator/chatbot-ui/js/home.js
+++ navigator/chatbot-ui/js/home.js
@@ -1,6 +1,53 @@
 function loadRegister(){
     $("#content").load("html/register.html");
 }
+function acceptForm()
+{
+  // alert("helloooooooo fhgn gg ");
+   var nm = document.getElementById('inputName').value;
+   var pss = document.getElementById('inputPassword4').value;
+   var eml = document.getElementById('inputEmail4').value;
+   var add = document.getElementById('inputAddress').value;
+   var cnt = document.getElementById('inputContact').value;
+
+   
+      /*alert(nm);
+      alert(pss);
+      alert(eml);
+      alert(add);
+      alert(cnt);
+   */
+   var ar = new Array(nm,pss,eml,add,cnt);
+   var obj = { 
+        name :nm,
+        emailid :eml,
+        contact :cnt,
+        pass:pss,
+        addrs:add};
+
+        var sendInfo = JSON.stringify(obj);
+            
+        alert(sendInfo);
+
+         $.ajax({
+           type: "POST",
+           url: "http://localhost:8080/register",
+            contentType: "application/json;charset=utf-8",
+           dataType: "json",
+           success: function () {
+                   alert("Somebody" + nm+ " was added in list !");
+               },
+
+           data: sendInfo
+       });
+         alert("done");
+};
+
+
+
+
+
+  
 
 
 $(document).ready(function() {
@@ -13,34 +60,7 @@ $(document).ready(function() {
     $("#register").click(function(){
 
 
-                    var nm = document.getElementById('inputName').value;
-                    var pass  = document.getElementById('inputPassword4').value;
-                    var eml = document.getElementById('inputEmail4').value;
-                    var add =document.getElementById('inputAddress').value;
-                    var cnt = document.getElementById('inputContact').value;
-
-
-
-              
-                    var  reg={
-                    "name" :nm,
-                    "emailid" :eml,
-                    "contact" :cnt,
-                    "pass" :pass,
-                    "addrs":add
-                        };
-               
-                   $.ajax({
-                    url: "localhost:8080/register",
-                    type: "POST",
-                    data: reg,
-                    contentType: "application/json",
-                    success: function (data) {
-                        callback(data);
-                        }
-                    });
-
-
+                   acceptForm();
 
 
 
